359dca435a10f39fa08f41c4f38cb8facd012f04
GianniLunardi <gianni.lunardi96@gmail.com>
2024-01-02 10:31:47 +0100

    add definitions

---
+++ src/defines.h
@@ -0,0 +1,19 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define APP_NAME "telemetry"
+#define APP_DOMAIN "it.unitn.telemetry"
+#define APP_VERSION "0.1.0"
+
+#define SOCKET_TIMEOUT 200 // Milliseconds
+#define CONNECT_DELAY 200000 // Microseconds
+
+#define DEFAULT_ENDPOINT_BIND "tcp://*:9000"
+#define DEFAULT_ENDPOINT "tcp://localhost:9000"
+#define DEFAULT_TOPIC "telemetry"
+#define FORMAT_PLAIN "plain"
+#define FORMAT_COMPRESSED "snappy"
+
+
+
+#endif // DEFINES_H
